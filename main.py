a130ce1198ce36eadd0706d9edd797c23f86a2c6
Svahnen <svahn.jonny@gmail.com>
2022-09-19 13:52:36 +0200

    Publish .

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import RPi.GPIO as GPIO
+import paho.mqtt.client as mqtt 
 GPIO.setmode(GPIO.BOARD)
 
 buttonPin = 16
 
+mqttBroker ="35.228.72.67" 
+
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # sätter inbyggd resistor så man inte behöver koppla en egen
+
+client = mqtt.Client("jonny_pi")
+client.connect(mqttBroker) 
 
 pressed = False
 
@@ -14,5 +20,6 @@ while True:
 		if pressed == False:
 			pressed = True
 			print("pressed")
+			client.publish("morse", ".")
 	else:
 		pressed = False
