9dda38c5890b7a295f398f07e519a10fe665edda
Svahnen <svahn.jonny@gmail.com>
2022-09-21 10:20:06 +0200

    Comments and cleanup

--- main.py
+++ main.py
@@ -1,36 +1,36 @@
 import RPi.GPIO as GPIO
 import paho.mqtt.client as mqtt
-import time 
+import time
 GPIO.setmode(GPIO.BOARD)
 
-buttonPin = 16
+buttonPin = 16  # GPIO 23
 
-mqttBroker ="35.228.72.67" 
+mqttBroker = "35.228.72.67"  # IP address of the MQTT broker
 
 GPIO.setup(buttonPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-# sätter inbyggd resistor så man inte behöver koppla en egen
+# Enable pull up resistor on button so we dont need an external resistor
 
-client = mqtt.Client("jonny_pi")
-#client.connect(mqttBroker) 
+client = mqtt.Client("jonny_pi")  # Set client name for MQTT
 
-pressed = False
+pressed = False  # Variable to keep track of button state
 
-loops = 0
+loops = 0  # Variable to keep track of how many times the loop has run
 
 while True:
-	client.connect(mqttBroker)
-	buttonState = GPIO.input(buttonPin)
-	if buttonState == False:
-		time.sleep(0.1)
-		loops+=1
-		if pressed == False:
-			pressed = True
-	else:
-		if loops > 5 and pressed == True:
-			print("long")
-			client.publish("morse", "_")
-		elif pressed == True:
-			print("short")
-			client.publish("morse", ".")
-		pressed = False
-		loops = 0
+    client.connect(mqttBroker)
+    buttonState = GPIO.input(buttonPin)
+    if buttonState == False:
+        # Debounce and make sure every loop takes at least 0.1 seconds
+        time.sleep(0.1)
+        loops += 1
+        if pressed == False:
+            pressed = True
+    else:
+        if loops > 5 and pressed == True:  # If the button has been pressed for more than 5 = 0.5 seconds send long
+            print("long")
+            client.publish("morse", "_")  # Publish to topic morse
+        elif pressed == True:  # If the button has been pressed for less, send short
+            print("short")
+            client.publish("morse", ".")  # Publish to topic morse
+        pressed = False  # Reset pressed variable
+        loops = 0  # Reset loops
